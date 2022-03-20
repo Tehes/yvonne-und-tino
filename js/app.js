7fbb4ee82982153cd6d60640755d3d97c82b7257
Tino Sabetta <tino.sabetta@gmail.com>
2022-03-20 16:33:24 +0100

    changed map to an interactive one.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ Variables
 var contactForm = document.querySelector("#kontakt form");
 var selectAdults = document.querySelector("#adults");
 var selectChildren = document.querySelector("#children");
+var osm = document.querySelector("#osm");
 
 /* --------------------------------------------------------------------------------------------------
 functions
@@ -17,13 +18,13 @@ function pickRecipient() {
 }
 
 function addNameFields() {
-	var inputFields, newInput; 
-	
+	var inputFields, newInput;
+
 	inputFields = document.getElementsByClassName(event.target.id);
 	if (event.target.value === 0) {
 		inputFields.length = 0;
 	}
-	
+
 	while (event.target.value > inputFields.length){
 		newInput = document.createElement("input");
 		newInput.type = "text";
@@ -33,18 +34,30 @@ function addNameFields() {
 		newInput.required = true;
 		document.querySelector("#"+event.target.id).after(newInput);
 	}
-	
+
 	while (event.target.value < inputFields.length){
 		inputFields[inputFields.length-1].remove();
 	}
+}
+
+function activateScroll() {
+    osm.classList.remove("no_scroll");
+}
+
+function deactivateScroll() {
+    osm.classList.add("no_scroll");
 }
 
 
 function init() {
     contactForm[0].addEventListener("change", pickRecipient, false);
 	selectAdults.addEventListener("change", addNameFields, false);
-	if (selectChildren) { 
+	if (selectChildren) {
 		selectChildren.addEventListener("change", addNameFields, false);
+	}
+    if (osm) {
+		document.body.addEventListener("click", activateScroll, false);
+		window.addEventListener("scroll", deactivateScroll, false);
 	}
 }
 
